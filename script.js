fa62fb6a2b4d0a17486c992eb65e6156d7f7918f
Doyo Abdikadir <doyo.doyo@student.moringaschool.com>
2022-03-06 21:26:45 +0300

    My submit button is now responsive

--- script.js
+++ script.js
@@ -4,12 +4,13 @@ let Year=document.getElementById("enterYear");
 
 var form= document.getElementById("submit");
 document.getElementById("btn").addEventListener('click', e => {    
-    let CC = enterYear.value.slice(0,2);
+    let CC = enterYear.value.substring(0,2);
     let YY = enterYear.value.substring(2);
     let MM = enterMonth.value;
     let DD = enterDay.value;
-    d = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7
+    d = Math.floor ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD )% 7;
+
     console.log(d);
  
-});
+})
 
